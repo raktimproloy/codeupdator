9028acf0aa26bbd35d2b1f8acae8e0bb00609842
raktimproloy <raktimproloy@gmail.com>
2023-11-19 23:56:33 +0600

    update navbar design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { PT_Sans } from 'next/font/google'
 import Navbar from "@/components/shared/Navbar"
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const pt_sans = PT_Sans({ weight: ['400', "700"], subsets: ['cyrillic'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,11 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.css"  rel="stylesheet" />
+      {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.css"  rel="stylesheet" /> */}
       </head>
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={pt_sans.className} suppressHydrationWarning={true}>
         <Navbar/>
-        <div className='w-[90%] mx-auto md:w-[60%]'>
+        <div className='max-w-6xl m-auto px-5'>
           {children}
         </div>
         <script async src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js"></script>
