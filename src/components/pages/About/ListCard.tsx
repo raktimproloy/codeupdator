85bad386d5c0fcd54efe084ecad89ca7e603840c
Sudipto Laskar <sudiptolaskar01@gmail.com>
2024-01-10 00:59:57 +0600

    change top rated design

--- src/components/pages/About/ListCard.tsx
+++ src/components/pages/About/ListCard.tsx
@@ -1,20 +1,35 @@
 import Interest from '@/components/shared/Profile/Interest'
+import { Location, Email } from '@/store/icons/Icons'
 import React from 'react'
 
 function ListCard() {
   return (
-    <div className='flex items-center my-5 shadow rounded p-3'>
-        <div className="avatar">
-            <div className="w-20 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    <>
+      <div className='shadow rounded p-3'>
+        <div className='flex items-center'>
+            <div className="avatar">
+                <div className="w-20 rounded-full">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+            <div className='ml-5'>
+                <h1 className='font-bold text-2xl'>Joshim Uddin</h1>
+                <p>@joshim_laravelest</p>
+                <div className='flex mt-2 items-center text-gray-600'>
+                  <Location/>
+                  <span className='ml-1'>Bangladesh</span>
+                </div>
+                <div className='hidden'>
+                  <Interest/>
+                </div>
             </div>
         </div>
-        <div className='ml-5'>
-            <h1 className='font-bold text-2xl'>Joshim Uddin</h1>
-            <p>@joshim_laravelest</p>
-            <Interest/>
+        <div className='mt-3'>
+          <p className='mb-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit...</p>
+          <Interest/>
         </div>
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/store/icons/Icons.tsx
+++ src/store/icons/Icons.tsx
@@ -154,8 +154,23 @@ export const RightArrow = () => {
     return (
         <>
             <svg xmlns="http://www.w3.org/2000/svg" width={"30px"} height={"30px"} viewBox="0 -960 960 960" ><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
-            
-{/* <svg xmlns="http://www.w3.org/2000/svg" width={"30px"} height={"30px"} viewBox="0 -960 960 960"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg> */}
         </>
     )
 }
+
+export const Location = () => {
+    return (
+        <>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width={"20px"} height={"20px"} viewBox="0 -960 960 960"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z"/></svg>
+        </>
+    )
+}
+
+export const Email = () => {
+    return (
+        <>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width={"20px"} height={"20px"} viewBox="0 -960 960 960"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z"/></svg>
+        </>
+    )
+}
+
