7134f90c2ec57cf86ac0efe88f3f6fa02f1f6c3c
Joshim Uddin <idkhs8211@gmail.com>
2024-01-21 23:12:45 +0600

    added some design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bc624649470d22e"; }
+/******/ 		__webpack_require__.h = function() { return "7f5ac4b4db2d6fc0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
