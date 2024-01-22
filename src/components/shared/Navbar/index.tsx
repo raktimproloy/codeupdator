1059a605e971cc1dad37a8113236e74b390f8ccb
Sudipto Laskar <sudiptolaskar01@gmail.com>
2024-01-22 23:15:13 +0600

    added navbar

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -1,282 +1,107 @@
-import React from "react";
+import React from 'react'
 import Link from "next/link";
-import MobileNav from "./MobileNav";
-import Search from "./Search";
-import Dropdown from "./Dropdown";
 import Image from "next/image";
 
-// Images
-import DefaultProfileImage from "/public/images/default-profile.png";
+import Search from './Search';
+
 import Logo from "/public/images/logo.svg";
+import DefaultProfileImage from "/public/images/default-profile.png";
 
 function index() {
   return (
-    <>
-      {/* <nav className="bg-white border-gray-200 dark:bg-gray-900 "> */}
-      <nav className="flex-no-wrap fixed top-0 z-10 w-full items-center justify-between bg-[#FBFBFB] shadow-md shadow-black/2 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-1 bg-white border-gray-200 dark:bg-gray-900 ">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          {/* Logo Component */}
-          <Link
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse hidden min-[425px]:flex"
-          >
-            <Image src={Logo} width={300} height={300} alt="logo" />
-            {/* <img src={Logo} className="h-8" alt="Flowbite Logo" /> */}
-            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
-          </Link>
-
-          {/* Search Component */}
-          <div className="flex md:order-2 min-[425px]:hidden md:flex">
-            <Search />
-          </div>
-
-          {/* Menu Component */}
-          <div
-            id="mega-menu-icons"
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          >
-            <ul className="flex justify-center items-center mt-4 font-medium md:mt-0 md:space-x-4 xl:space-x-8 rtl:space-x-reverse">
-              <li>
-                <Link
-                  href="/"
-                  className="block py-2 px-3 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                {/* <div className="dropdown dropdown-bottom dropdown-end ">
-                            <div tabIndex={0} role="button" className="">
-                            <div className=" font-medium text-gray-900 cursor-pointer" title="ThreeDot">
-                                Category
-                            </div>
-                            </div>
-                            <Dropdown/>
-                        </div> */}
-
-                <div className="navbar bg-base-100">
-                  <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal">
-                      <li>
-                        <details>
-                          <summary>Category</summary>
-                          <ul className="p-2">
-                            <li>
-                              <a>
-                                <div className="dropdown dropdown-bottom dropdown-end ">
-                                  <div tabIndex={0} role="button" className="">
-                                    <div
-                                      className=" font-medium text-gray-900 cursor-pointer"
-                                      title="ThreeDot"
-                                    >
-                                      Category
-                                    </div>
-                                  </div>
-                                  <Dropdown />
-                                </div>
-                              </a>
-                            </li>
-                            <li>
-                              <a>Framework</a>
-                            </li>
-                            <li>
-                              <a>Library</a>
-                            </li>
-                          </ul>
-                        </details>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                {/* <button id="mega-menu-icons-dropdown-button" data-dropdown-toggle="mega-menu-icons-dropdown" className="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                Category 
-                                <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                                </svg>
-                            </button> */}
-              </li>
-              <li>
-                <Link
-                  href="/problem"
-                  className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Problem
-                </Link>
-              </li>
-              {/* MVP(Most valueable programmer)  heroes */}
-              <li>
-                <Link
-                  href="/about"
-                  className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Top Rated
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Blog
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Profile Icon */}
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-2 ">
-            <button
-              type="button"
-              className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown-desktop"
-              data-dropdown-placement="bottom"
-            >
-              <span className="sr-only">Open user menu</span>
-              <Image
-                className="w-10 h-10 rounded-full"
-                src={DefaultProfileImage}
-                width={100}
-                height={100}
-                alt="profile"
-              />
-            </button>
-
-            <div
-              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              id="user-dropdown-desktop"
-            >
-              <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
-                </span>
-                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                  name@flowbite.com
-                </span>
-              </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
+    <div className='shadow-md shadow-black/10 fixed top-0 z-10 w-full'>
+        <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
+        <div className="navbar-start">
+            {/* <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-          </div>
-
-          {/* Mobile Menu Icons */}
-          <div className="flex items-center md:w-full md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-            <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse md:hidden">
-              {/* Serch Icon */}
-              <div className="md:order-2 hidden min-[425px]:flex">
-                <Search />
-              </div>
-
-              {/* Profile Icon */}
-              <button
-                type="button"
-                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                id="user-menu-button"
-                aria-expanded="false"
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom"
-              >
-                <span className="sr-only">Open user menu</span>
-                <Image
-                  className="w-10 h-10 rounded-full"
-                  src={DefaultProfileImage}
-                  width={100}
-                  height={100}
-                  alt="profile"
-                />
-              </button>
-
-              {/* <!-- Dropdown menu --> */}
-              <div
-                className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                id="user-dropdown"
-              >
-                <div className="px-4 py-3">
-                  <span className="block text-sm text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </span>
-                  <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                    name@flowbite.com
-                  </span>
-                </div>
-                <ul className="py-2" aria-labelledby="user-menu-button">
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Sign out
-                    </a>
-                  </li>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
                 </ul>
-              </div>
-            </div>
-          </div>
+                </li>
+                <li><a>Item 3</a></li>
+            </ul>
+            </div> */}
+            {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
+            <Link
+                href="/"
+                className="flex items-center space-x-3 rtl:space-x-reverse hidden min-[425px]:flex"
+            >
+                <Image src={Logo} width={300} height={300} alt="logo" />
+            </Link>
         </div>
-
-        <MobileNav />
-      </nav>
-    </>
-  );
+        <div className="navbar-center hidden md:flex">
+            <ul className="menu menu-horizontal px-1">
+            <li><a>Item 1</a></li>
+            <li>
+                <details>
+                <summary>Parent</summary>
+                <ul className="menu md:menu-horizontal md:min-w-max bg-base-400 rounded transform translate-x-[-43%] translate-y-[2px]" style={{padding:"2rem"}}>
+                    <li className='px-7'>
+                        <a className='text-xl font-bold menu-title'>Language</a>
+                        <ul>
+                        <li><a>Javascript</a></li>
+                        <li><a>PHP</a></li>
+                        <li><a>Python</a></li>
+                        </ul>
+                    </li>
+                    <li className='px-7'>
+                        <a className='text-xl font-bold menu-title'>Frontend</a>
+                        <ul>
+                        <li><a>ReactJs</a></li>
+                        <li><a>NextJs</a></li>
+                        <li><a>VueJs</a></li>
+                        </ul>
+                    </li>
+                    <li className='px-7'>
+                        <a className='text-xl font-bold menu-title'>Backend</a>
+                        <ul>
+                        <li><a>Laravek</a></li>
+                        <li><a>NodeJs</a></li>
+                        </ul>
+                    </li>
+                </ul>
+      
+                </details>
+            </li>
+            <li><a>Item 3</a></li>
+            </ul>
+        </div>
+        <div className="navbar-end">
+            <Search />
+            <div className="dropdown dropdown-end ml-5 bg-gray-800 rounded-full">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    <div className="w-9 rounded-full">
+                        <Image
+                            className="w-9 h-9 rounded-full"
+                            src={DefaultProfileImage}
+                            width={100}
+                            height={100}
+                            alt="profile"
+                        />
+                    </div>
+                </div>
+                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded w-52">
+                    <li>
+                    <a className="justify-between">
+                        Profile
+                        <span className="badge">New</span>
+                    </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
 }
 
-export default index;
+export default index
