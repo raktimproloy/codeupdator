e63da0a64e88a53d34eb346bf69cadaeb9e09670
Joshim Uddin <idkhs8211@gmail.com>
2024-01-23 01:02:59 +0600

    added some design

--- src/components/pages/Contact/index.tsx
+++ src/components/pages/Contact/index.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 function index() {
   return (
-      <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse justify-between">
     <div className="text-center lg:text-left">
     <Image src={ContactUsPhoto} width={1080} height={1920} alt="logo" />
 
