dcecd6a55c35cee3206bee8fc6a13de5acfcc2b2
Joshim Uddin <idkhs8211@gmail.com>
2024-01-23 22:59:03 +0600

    added some design

--- src/components/pages/Contact/index.tsx
+++ src/components/pages/Contact/index.tsx
@@ -11,25 +11,31 @@ function index() {
     <Image src={ContactUsPhoto} width={1080} height={1920} alt="logo" />
 
     </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="rounded shrink-0 w-full max-w-sm shadow bg-base-100">
       <form className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="Emain" className="rounded input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Phon Number (Optional)</span>
+          </label>
+          <input type="text" placeholder="Number" className="rounded input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Message</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <textarea placeholder="Your message" className="rounded textarea textarea-bordered" required></textarea>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">Send</button>
         </div>
       </form>
     </div>
