c2cfb5f3b593ec3a1afba2902d6668bba6924705
Sudipto Laskar <sudiptolaskar01@gmail.com>
2024-01-27 01:08:01 +0600

    added category close func

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from "next/link";
 import Image from "next/image";
 
@@ -12,6 +12,7 @@ import DefaultProfileImage from "/public/images/default-profile.png";
 
 function Index() {
     const [theme, setTheme] = useState("")
+    const detailsRef = useRef<HTMLDetailsElement | null>(null);
     useEffect(() => {
         const localTheme = localStorage.getItem("_theme") || "light"
         setTheme(localTheme)
@@ -25,7 +26,27 @@ function Index() {
         setTheme(newTheme);  // Update the theme state
     };
 
+    const handleClickOutside = (event: MouseEvent) => {
+        if (detailsRef.current && !detailsRef.current.contains(event.target as Node)) {
+          // Clicked outside the details tag, close it
+          detailsRef.current.removeAttribute('open');
+        }
+      };
+    
+      useEffect(() => {
+        // Add event listener on mount
+        document.addEventListener('click', handleClickOutside);
+    
+        // Clean up the event listener on unmount
+        return () => {
+          document.removeEventListener('click', handleClickOutside);
+        };
+      }, []);
 
+      const handleLinkClick = (event: React.MouseEvent) => {
+        // Stop propagation to prevent closing details when clicking on links
+        detailsRef.current?.removeAttribute('open');
+      };
   return (
     <>
         <div className='shadow-md fixed top-0 z-10 w-full bg-base-100'>
@@ -42,33 +63,33 @@ function Index() {
                     <ul className="menu menu-horizontal px-1">
                     <li><Link href={"/"} className='text-base hover:bg-inherit hover:text-red-500'>Home</Link></li>
                     <li>
-                        <details>
-                        <summary className='text-base hover:bg-inherit active:text-red-500 hover:text-red-500'>Category</summary>
-                        <ul className="menu md:menu-horizontal md:min-w-max bg-base-400 rounded transform translate-x-[-43%] translate-y-[2px]" style={{padding:"1rem"}}>
-                            <li className='pr-7'>
-                                <p className='text-lg font-bold menu-title'>Language</p>
-                                <ul>
-                                <li><Link href={"/category/javascript"}>Javascript</Link></li>
-                                <li><Link href={"/category/php"}>PHP</Link></li>
-                                <li><Link href={"/category/python"}>Python</Link></li>
-                                </ul>
-                            </li>
-                            <li className='px-7'>
-                                <p className='text-lg font-bold menu-title'>Frontend</p>
-                                <ul>
-                                <li><Link href={"/category/reactjs"}>ReactJs</Link></li>
-                                <li><Link href={"/category/nextjs"}>NextJs</Link></li>
-                                <li><Link href={"/category/vuejs"}>VueJs</Link></li>
-                                </ul>
-                            </li>
-                            <li className='px-7'>
-                                <p className='text-lg font-bold menu-title'>Backend</p>
-                                <ul>
-                                <li><Link href={"/category/laravel"}>Laravel</Link></li>
-                                <li><Link href={"/category/nodejs"}>NodeJs</Link></li>
-                                </ul>
-                            </li>
-                        </ul>
+                        <details ref={detailsRef}>
+                            <summary className='text-base hover:bg-inherit active:text-red-500 hover:text-red-500'>Category</summary>
+                            <ul className="menu md:menu-horizontal md:min-w-max bg-base-400 rounded transform translate-x-[-43%] translate-y-[2px]" style={{padding:"1rem"}}>
+                                <li className='pr-7'>
+                                    <p className='text-lg font-bold menu-title'>Language</p>
+                                    <ul>
+                                    <li onClick={handleLinkClick}><Link href={"/category/javascript"}>Javascript</Link></li>
+                                    <li onClick={handleLinkClick}><Link href={"/category/php"}>PHP</Link></li>
+                                    <li onClick={handleLinkClick}><Link href={"/category/python"}>Python</Link></li>
+                                    </ul>
+                                </li>
+                                <li className='px-7'>
+                                    <p className='text-lg font-bold menu-title'>Frontend</p>
+                                    <ul>
+                                    <li onClick={handleLinkClick}><Link href={"/category/reactjs"}>ReactJs</Link></li>
+                                    <li onClick={handleLinkClick}><Link href={"/category/nextjs"}>NextJs</Link></li>
+                                    <li onClick={handleLinkClick}><Link href={"/category/vuejs"}>VueJs</Link></li>
+                                    </ul>
+                                </li>
+                                <li className='px-7'>
+                                    <p className='text-lg font-bold menu-title'>Backend</p>
+                                    <ul>
+                                    <li onClick={handleLinkClick}><Link href={"/category/laravel"}>Laravel</Link></li>
+                                    <li onClick={handleLinkClick}><Link href={"/category/nodejs"}>NodeJs</Link></li>
+                                    </ul>
+                                </li>
+                            </ul>
             
                         </details>
                     </li>
