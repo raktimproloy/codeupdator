9785dd47f0210cb1299fe8021aa626f7452c3668
Sudipto Laskar <sudiptolaskar01@gmail.com>
2024-05-09 01:35:01 +0600

    added api and login func

--- src/components/shared/Navbar/MobileMenu.tsx
+++ src/components/shared/Navbar/MobileMenu.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { About, Category, Contact, Home, Problem } from '@/store/icons/Icons'
 import Styles from "./style.module.css"
+import Link from 'next/link'
 
 function MobileMenu() {
   return (
     <div className='grid grid-cols-5 gap-3 md:hidden py-2 bg-white'>
-        <div className={`flex justify-center border-r text-gray-700 ${Styles.iconStyle}`}>
+        <Link href={"/"} className={`flex justify-center border-r text-gray-700 ${Styles.iconStyle}`}>
             <Home/>
-        </div>
-        <div className={`flex justify-center border-r ${Styles.iconStyle}`}>
+        </Link>
+        <Link href={"/problem"} className={`flex justify-center border-r ${Styles.iconStyle}`}>
             <Category/>
-        </div>
-        <div className={`flex justify-center border-r ${Styles.iconStyle}`}>
+        </Link>
+        <Link href={"/top-rated"} className={`flex justify-center border-r ${Styles.iconStyle}`}>
             <Problem/>
-        </div>
-        <div className={`flex justify-center border-r ${Styles.iconStyle}`}>
+        </Link>
+        <Link href={"/about"} className={`flex justify-center border-r ${Styles.iconStyle}`}>
             <About/>
-        </div>
-        <div className={`flex justify-center ${Styles.iconStyle}`}>
+        </Link>
+        <Link href={"/contact"} className={`flex justify-center ${Styles.iconStyle}`}>
             <Contact/>
-        </div>
+        </Link>
     </div>
   )
 }
